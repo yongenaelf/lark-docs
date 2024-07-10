b1a36333654675d7497a7d99cf535c5f701261e3
AbigailDeng <Abigail.deng@ienyan.com>
2024-07-10 19:24:12 +0800

    feat: menu & sidebar & Breadcrumb

---
+++ lib/api.ts
@@ -0,0 +1,10 @@
+export const fetcher = async (url: string) => {
+  const res = await fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${process.env.USER_TOKEN}`,
+    },
+    next: { revalidate: 1 },
+  });
+  return res.json();
+};
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,53 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { NodesData, NodesItem } from "../services/larkServices";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function findTopLevelItems(data: NodesData, key: string) {
+  function search(items: NodesItem[], key: string) {
+    for (let item of items) {
+      if (item.obj_token === key) {
+        return true;
+      }
+      if (item.children) {
+        for (let child of item.children) {
+          if (search(child.items, key)) {
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  }
+  for (let item of data.items) {
+    if (search([item], key)) {
+      return [item];
+    }
+  }
+  return null;
+}
+export function findPathByKey(data: NodesData, key: string) {
+  let path: NodesItem[] = [];
+
+  function find(items: NodesItem[]) {
+    for (let item of items) {
+      if (item.obj_token === key) {
+        path.push(item);
+        return true;
+      } else if (item.has_child && item.children.length > 0) {
+        for (let child of item.children) {
+          if (find(child.items)) {
+            path.unshift(item); // Add item to the beginning of path array
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  }
+  find(data.items);
+  return path.length > 0 ? path : null;
 }
