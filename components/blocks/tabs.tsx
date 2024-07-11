01d21ff64e288e0238c96d3e14e716eaf2bf657b
yongen.loong <yongen.loong@aelf.io>
2024-07-11 20:59:03 +0800

    fix: tabs

--- components/blocks/tabs.tsx
+++ components/blocks/tabs.tsx
@@ -5,7 +5,7 @@ import {
   TabsTrigger,
 } from "@/components/ui/tabs";
 import { Item } from "./common";
-import Renderer from "./renderer";
+import Renderer, { AnyItem } from "./renderer";
 
 export interface Tabs extends Item {
   block_type: 24;
@@ -23,42 +23,40 @@ export interface Tab extends Item {
   };
 }
 
+const getFirstItem = (id: string, items: AnyItem[]) => {
+  const firstId = (items.find((i) => i.block_id === id) as Tab).children[0];
+
+  const firstItem = items.find((i) => i.block_id === firstId);
+
+  if (firstItem?.block_type === 2) {
+    return firstItem;
+  }
+
+  return undefined;
+};
+
+const getAllTabChildrenExceptFirst = (id: string, items: AnyItem[]) => {
+  const allChildren = (items.find((i) => i.block_id === id) as Tab).children;
+
+  const allItems = items
+    .filter((i) => allChildren.includes(i.block_id))
+    .slice(1);
+
+  return allItems;
+};
+
 export function Tabs(props: Tabs) {
   const allChildren = props.allItems.filter((i) =>
     props.children.includes(i.block_id)
   ) as Tab[];
-
-  const getFirstItem = (id: string) => {
-    const firstId = (props.allItems.find((i) => i.block_id === id) as Tab)
-      .children[0];
-
-    const firstItem = props.allItems.find((i) => i.block_id === firstId);
-
-    if (firstItem?.block_type === 2) {
-      return firstItem;
-    }
-
-    return undefined;
-  };
-
-  const getAllTabChildrenExceptFirst = (id: string) => {
-    const allChildren = (props.allItems.find((i) => i.block_id === id) as Tab)
-      .children;
-
-    const allItems = props.allItems
-      .filter((i) => allChildren.includes(i.block_id))
-      .slice(1);
-
-    return allItems;
-  };
 
   return (
     <TabsLib defaultValue="account" className="mt-8">
       <TabsList>
         {allChildren.map((i, index) => {
           const value =
-            getFirstItem(i.block_id)?.text.elements?.[0].text_run.content ||
-            `Tab ${index}`;
+            getFirstItem(i.block_id, props.allItems)?.text.elements?.[0]
+              .text_run.content || `Tab ${index}`;
           return (
             <TabsTrigger key={i.block_id} value={value}>
               {value}
@@ -67,13 +65,17 @@ export function Tabs(props: Tabs) {
         })}
       </TabsList>
       {allChildren.map((i, index) => {
-        const allChildren = getAllTabChildrenExceptFirst(i.block_id);
+        const allChildren = getAllTabChildrenExceptFirst(
+          i.block_id,
+          props.allItems
+        );
         const value =
-          getFirstItem(i.block_id)?.text.elements?.[0].text_run.content ||
-          `Tab ${index}`;
+          getFirstItem(i.block_id, props.allItems)?.text.elements?.[0].text_run
+            .content || `Tab ${index}`;
 
         return (
           <TabsContent
+            key={i.block_id}
             value={value}
             className="rounded-md border px-4 pb-2 mb-8"
           >
